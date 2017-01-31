1182eb6b5feb1be6e9f916ace4044e0928d0c993
Sidney <sidneydegeus@hotmail.com>
2017-01-31 15:17:41 +0100

    add lesson 1 of C++ and AI to git

---
+++ AI Algoritme/Maze/src/Application.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Sidney on 1/31/2017.
+ */
+public class Application {
+}
---
+++ C++/Lecture1.1/Lecture1.1/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int sum(int input1, int input2);
+
+int main() {
+	cout << "Sum of 3 and 5 is: " << sum(3, 5);
+	return 0;
+}
+
+int sum(int input1, int input2) {
+	return input1 + input2;
+}
+
+
+
+
