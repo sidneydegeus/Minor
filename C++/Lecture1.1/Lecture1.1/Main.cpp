a3dc561e148317c3659e1967529f321e4d3f43c4
Sidney <sidneydegeus@hotmail.com>
2017-01-31 15:31:40 +0100

    added more shit

--- C++/Lecture1.1/Lecture1.1/Main.cpp
+++ C++/Lecture1.1/Lecture1.1/Main.cpp
@@ -2,14 +2,25 @@
 using namespace std;
 
 int sum(int input1, int input2);
+string evenOrOdd(int input);
 
 int main() {
-	cout << "Sum of 3 and 5 is: " << sum(3, 5);
+	cout << "Sum of 3 and 5 is: " << sum(3, 5) << endl;
 	return 0;
 }
 
 int sum(int input1, int input2) {
 	return input1 + input2;
+}
+
+string evenOrOdd(int input) {
+	string result;
+	if (input % 2 == 0) {
+		result = "Even number";
+	} else {
+		result = "Uneven number";
+	}
+	return result;
 }
 
 
