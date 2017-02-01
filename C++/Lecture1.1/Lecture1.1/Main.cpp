73a7b0f79bd4a2211d6c63ac056016c3bba04a4f
Sidney <sidneydegeus@hotmail.com>
2017-02-01 04:15:53 +0100

    add more stuff to lecture 1.1

--- C++/Lecture1.1/Lecture1.1/Main.cpp
+++ C++/Lecture1.1/Lecture1.1/Main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <string>
+#include <math.h>
 using namespace std;
 
 int sum(int input1, int input2);
 string evenOrOdd(int input);
+int exponentFunc(int base, int exponent);
+string isPrimeNumber(int input);
 
 int main() {
 	cout << "Sum of 3 and 5 is: " << sum(3, 5) << endl;
+	cout << "Number 5 is an " << evenOrOdd(5) << endl;
+	cout << "Number 4 is an " << evenOrOdd(4) << endl;
+	cout << "exponent of 5^4 is " << exponentFunc(5, 4) << endl;
+	cout << "6 is " << isPrimeNumber(6) << endl;
+	cout << "7 is " << isPrimeNumber(7) << endl;
 	return 0;
 }
 
@@ -17,12 +26,33 @@ string evenOrOdd(int input) {
 	string result;
 	if (input % 2 == 0) {
 		result = "Even number";
-	} else {
+	} 
+	else {
 		result = "Uneven number";
 	}
 	return result;
 }
 
+int exponentFunc(int base, int exponent) {
+	cout << exponent << endl;
+	if (exponent >= 1) {
+		return base * (exponentFunc(base, exponent-1));
+	}
+	else {
+		return 1;
+	}
+}
 
+string isPrimeNumber(int input) {
+	if (input < 2) {
+		return "Not a prime number";
+	}
+	for (int i = 2; i < sqrt(input); i++) {
+		if ((input % i) == 0) {
+			return "Not a prime number";
+		}
+	}
+	return "a Prime number";
+}
 
 
